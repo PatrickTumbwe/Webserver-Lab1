22470a0fefe928b472f252f178b4ef4e9b1e18ce
Chansa26 <mwitaraphael15@gmail.com>
2024-08-19 18:15:12 +0200

    Made changes to allow POST using API

--- WebServerApp/views.py
+++ WebServerApp/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import TemperatureReading
-from .serializer import TemperatureReadingSerializer
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import TemperatureReading
+from .serializer import TemperatureReadingSerializer
+from django.shortcuts import render
 
 class TemperatureReadingList(generics.ListAPIView):
     queryset = TemperatureReading.objects.all()
@@ -16,6 +16,13 @@ class TemperatureReadingDetail(generics.RetrieveAPIView):
 class TemperatureCreateAPIView(generics.CreateAPIView):
     queryset = TemperatureReading.objects.all()
     serializer_class = TemperatureReadingSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 class LatestTemperatureAPIView(APIView):
     def get(self, request):
