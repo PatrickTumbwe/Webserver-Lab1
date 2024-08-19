38e562080027379b0b5083b288119290dde5c8d7
Chansa26 <mwitaraphael15@gmail.com>
2024-08-19 18:21:29 +0200

    Included the Function-based view for handling both GET and POST requests

--- WebServerApp/views.py
+++ WebServerApp/views.py
@@ -1,9 +1,9 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.shortcuts import render
+from rest_framework import generics
 from .models import TemperatureReading
 from .serializer import TemperatureReadingSerializer
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class TemperatureReadingList(generics.ListAPIView):
     queryset = TemperatureReading.objects.all()
@@ -17,18 +17,33 @@ class TemperatureCreateAPIView(generics.CreateAPIView):
     queryset = TemperatureReading.objects.all()
     serializer_class = TemperatureReadingSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
 class LatestTemperatureAPIView(APIView):
     def get(self, request):
         latest_temperature = TemperatureReading.objects.latest('id')
         serializer = TemperatureReadingSerializer(latest_temperature)
         return Response(serializer.data)
 
+# Function-based view for handling both GET and POST requests
+@api_view(['POST', 'GET'])
+def record_data(request):
+    if request.method == 'POST':
+        serializer = TemperatureReadingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'GET':
+        data = TemperatureReading.objects.all().order_by('-timestamp')[:10]
+        response_data = [
+            {
+                "temperature": entry.temperature,
+                "humidity": entry.humidity,
+                "timestamp": entry.timestamp,
+            }
+            for entry in data
+        ]
+        return Response({"data": response_data}, status=status.HTTP_200_OK)
+    
 def temperature_monitor(request):
     return render(request, 'home.html')
